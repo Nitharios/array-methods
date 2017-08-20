a3fa992cb940e621f25f847581a052e5882e3435
Nathan N <Nitharios@users.noreply.github.com>
2017-08-19 14:17:06 -1000

    Passed splice test

--- arrayMethods.js
+++ arrayMethods.js
@@ -32,3 +32,13 @@ var nextOrder = orderQueue.shift()
 mixedNums.sort()
 
 mixedWords.sort()
+
+var notFruit = fruitCollection.splice(1, 3)
+
+gemBox.splice(5, 2)
+
+gemBox.splice(6, 4)
+
+upToTen.splice(2, 0, 3, 4, 5, 6, 7, 8)
+
+brownOnly.splice(1, 5, 'brown', 'brown', 'brown')
